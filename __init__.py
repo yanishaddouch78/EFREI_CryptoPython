5e119ff255a4b7f0785029330ec25772902819b5
yanishaddouch78 <yanishaddouch@gmail.com>
2024-12-03 10:20:06 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -19,6 +19,15 @@ def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+
+@app.route('/decrypt/<string:token>')
+def decryptage(token):
+    try:
+        token_bytes = token.encode()  # Conversion str -> bytes
+        valeur_decrypt = f.decrypt(token_bytes)  # Décryptage du token
+        return f"Valeur décryptée : {valeur_decrypt.decode()}"  # Retourne la valeur en str
+    except Exception as e:
+        return f"Erreur lors du décryptage : {e}"
                                                                                                                                                      
 if __name__ == "__main__":
   app.run(debug=True)
